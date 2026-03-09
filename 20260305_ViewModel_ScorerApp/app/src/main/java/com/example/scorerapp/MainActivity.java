43e4a83097154fe47fa9654031c61d054e0091a7
Bernat <bernat.orellana@gmail.com>
2026-03-09 15:58:52 +0100

    View Model

--- 20260305_ViewModel_ScorerApp/app/src/main/java/com/example/scorerapp/MainActivity.java
+++ 20260305_ViewModel_ScorerApp/app/src/main/java/com/example/scorerapp/MainActivity.java
@@ -1,24 +1,35 @@
 package com.example.scorerapp;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.scorerapp.databinding.ActivityMainBinding;
+import com.example.scorerapp.viewmodel.MainActivityViewModel;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
 
-    int local=0;
-    int visitant=0;
+    private MainActivityViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Log.d("MainActivity", "onCreate");
+
         super.onCreate(savedInstanceState);
+
+        // carregar el view model
+        viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+
+
         //setContentView(R.layout.activity_main);
         // carreguem el layout amb el binding, i desem el binding en una variable global
         binding = ActivityMainBinding.inflate(getLayoutInflater());
@@ -26,21 +37,21 @@ public class MainActivity extends AppCompatActivity {
 
         mostrarScore();
 
-
         binding.btnLocalPlus.setOnClickListener(v -> {
-            local++;
+
+            viewModel.incLocal(1);
             mostrarScore();
         });
         binding.btnVisitorPlus.setOnClickListener(v -> {
-            visitant++;
+            viewModel.incVisitant(1);
             mostrarScore();
         });
         binding.btnLocalMinus.setOnClickListener(v -> {
-            local--;
+            viewModel.incLocal(-1);
             mostrarScore();
         });
         binding.btnVisitorMinus.setOnClickListener(v -> {
-            visitant--;
+            viewModel.incVisitant(-1);
             mostrarScore();
         });
 
@@ -48,8 +59,13 @@ public class MainActivity extends AppCompatActivity {
 
     private void mostrarScore() {
         //mostrar valors local i visitant als textboxes
-        binding.txvLocalScore.setText(String.valueOf(local));
-        binding.txvVisitorScore.setText(String.valueOf(visitant));
+        binding.txvLocalScore.setText(String.valueOf(viewModel.getLocal()));
+        binding.txvVisitorScore.setText(String.valueOf(viewModel.getVisitant()));
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        viewModel.save();
     }
 }
--- 20260305_ViewModel_ScorerApp/app/src/main/java/com/example/scorerapp/viewmodel/MainActivityViewModel.java
+++ 20260305_ViewModel_ScorerApp/app/src/main/java/com/example/scorerapp/viewmodel/MainActivityViewModel.java
@@ -1,4 +1,62 @@
 package com.example.scorerapp.viewmodel;
 
-public class MainActivityViewModel {
+import android.app.Activity;
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.ViewModel;
+
+public class MainActivityViewModel extends AndroidViewModel {
+    int local=0;
+    int visitant=0;
+
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+
+
+        Log.d("XXXXX", "Estic tornant a crear el ViewModel");
+        SharedPreferences sp = getApplication().getSharedPreferences("score", Activity.MODE_PRIVATE);
+        local = sp.getInt("local", 0);
+        visitant = sp.getInt("visitant", 0);
+
+
+    }
+
+    public int getLocal() {
+        return local;
+    }
+
+    public int getVisitant() {
+        return visitant;
+    }
+
+    //mètode per incrementar/decrementar el local
+    public void incLocal(int increment)
+    {
+        local+=increment;
+        if(local<0) local-=increment;
+    }
+
+    public void incVisitant(int increment)
+    {
+        visitant+=increment;
+        if(visitant<0) visitant-=increment;
+    }
+
+    public void reset(){
+        local=0;
+        visitant=0;
+    }
+
+    public void save() {
+        Log.d("MainActivity", "onPause");
+        SharedPreferences sp = getApplication().getSharedPreferences("score", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putInt("local", local);
+        editor.putInt("visitant", visitant);
+        editor.apply();
+    }
 }
